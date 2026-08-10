6e8575ead493034ff2dac749e04fd94d658bd2f9
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-10 10:25:33 +0300

    Add google and github oAuth environment variables to env.js setup

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -9,8 +9,21 @@ for (const variable of requiredEnvVariables) {
 }
 
 export const env = {
-  databaseUrl: process.env.DATABASE_URL,
-  jwtSecret: process.env.JWT_SECRET,
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT) || 3000,
+
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackUrl: process.env.GOOGLE_CALLBACK_URL,
+  },
+
+  github: {
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackUrl: process.env.GITHUB_CALLBACK_URL,
+  },
 };
