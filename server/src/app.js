7cdc2353beb925df2742cab76b9ca986650501de
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-10 10:06:16 +0300

    Create basic application setup for server

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Blog API is running",
+  });
+});
+
+export default app;
