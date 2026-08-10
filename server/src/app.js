7db174bf526f6648e5ae8fee9d0d03c033f56821
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-10 10:33:48 +0300

    Add passport to application

--- server/src/app.js
+++ server/src/app.js
@@ -3,13 +3,24 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import passport from "./config/passport/index.js";
+
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(morgan("dev"));
+
+app.use(passport.initialize());
 
 app.get("/health", (req, res) => {
   res.status(200).json({
