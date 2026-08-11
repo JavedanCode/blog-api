d463f6f1f11ba826804ed55f1c6975aff9b58886
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-11 11:18:24 +0300

    Normalize error handling in authenticate.js

--- server/src/middleware/authenticate.js
+++ server/src/middleware/authenticate.js
@@ -1,5 +1,31 @@
 import passport from "../config/passport/index.js";
 
-export const authenticate = passport.authenticate("jwt", {
-  session: false,
-});
+import { AppError } from "../errors/AppError.js";
+
+export function authenticate(req, res, next) {
+  passport.authenticate(
+    "jwt",
+    {
+      session: false,
+    },
+    (error, user) => {
+      if (error) {
+        return next(error);
+      }
+
+      if (!user) {
+        return next(
+          new AppError(
+            "Authentication required.",
+            401,
+            "INVALID_OR_MISSING_TOKEN",
+          ),
+        );
+      }
+
+      req.user = user;
+
+      next();
+    },
+  )(req, res, next);
+}
