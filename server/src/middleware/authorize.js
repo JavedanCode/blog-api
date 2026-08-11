5c03d529b6ec099edd3b78a6fa507bd20f93b019
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-11 11:18:46 +0300

    Normalize error handling in authorize.js

--- server/src/middleware/authorize.js
+++ server/src/middleware/authorize.js
@@ -1,15 +1,25 @@
+import { AppError } from "../errors/AppError.js";
+
 export function authorize(...allowedRoles) {
   return (req, res, next) => {
     if (!req.user) {
-      return res.status(401).json({
-        message: "Authentication required.",
-      });
+      return next(
+        new AppError(
+          "Authentication required.",
+          401,
+          "AUTHENTICATION_REQUIRED",
+        ),
+      );
     }
 
     if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({
-        message: "You do not have permission to perform this action.",
-      });
+      return next(
+        new AppError(
+          "You do not have permission to perform this action.",
+          403,
+          "FORBIDDEN",
+        ),
+      );
     }
 
     next();
